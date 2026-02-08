9cb8752b41bface8242c1a3c4dd1d5c29ee8db97
biruktadele <biruktadele55@gmail.com>
2026-02-08 11:15:44 +0300

    fete: implmentation of models analysis_response_model.dart

---
+++ hustlers_mobile/lib/features/analysis/data/models/analysis_response_model.dart
@@ -0,0 +1,25 @@
+import 'analysis_data_model.dart';
+
+class AnalysisResponseModel {
+  final String status;
+  final AnalysisDataModel data;
+
+  AnalysisResponseModel({
+    required this.status,
+    required this.data,
+  });
+
+  factory AnalysisResponseModel.fromJson(Map<String, dynamic> json) {
+    return AnalysisResponseModel(
+      status: json['status'],
+      data: AnalysisDataModel.fromJson(json['data']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'status': status,
+      'data': data.toJson(),
+    };
+  }
+}
