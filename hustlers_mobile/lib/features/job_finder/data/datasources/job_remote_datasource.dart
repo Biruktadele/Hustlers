dcb2f328ba49723db0ab5ecfdd1bfdb9f35ca68f
biruktadele <biruktadele55@gmail.com>
2026-02-08 11:15:46 +0300

    fete: implmentation of datasources job_remote_datasource.dart

---
+++ hustlers_mobile/lib/features/job_finder/data/datasources/job_remote_datasource.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/job_model.dart';
+
+abstract class JobRemoteDataSource {
+  Future<List<JobModel>> getJobs();
+}
+
+class JobRemoteDataSourceImpl implements JobRemoteDataSource {
+  final http.Client client;
+
+  JobRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<List<JobModel>> getJobs() async {
+    final response = await client.get(
+      Uri.parse('https://hustlers-production.up.railway.app/api/jobs'),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonResponse = json.decode(response.body);
+      if (jsonResponse['status'] == 'success') {
+        final List<dynamic> jobsJson = jsonResponse['jobs'];
+        return jobsJson.map((json) => JobModel.fromJson(json)).toList();
+      } else {
+        throw Exception('Failed to load jobs: API status not success');
+      }
+    } else {
+      throw Exception('Failed to load jobs');
+    }
+  }
+}
