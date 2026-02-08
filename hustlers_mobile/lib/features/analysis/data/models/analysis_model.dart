5bf42ec3081c94c56de468bc5d9bdc734b44c1f2
biruktadele <biruktadele55@gmail.com>
2026-02-08 11:15:44 +0300

    fete: implmentation of models analysis_model.dart

---
+++ hustlers_mobile/lib/features/analysis/data/models/analysis_model.dart
@@ -0,0 +1,8 @@
+export 'analysis_data_model.dart';
+export 'analysis_response_model.dart';
+export 'github_info_model.dart';
+export 'repo_evidence_model.dart';
+export 'repo_model.dart';
+export 'repo_scores_model.dart';
+export 'resume_info_model.dart';
+export 'suggestions_model.dart';
