f57e08eed81a84519ad8bd251d8fb042d06a4626
biruktadele <biruktadele55@gmail.com>
2026-02-09 18:01:14 +0300

    fete: implmentation of models job_model.dart

--- hustlers_mobile/lib/features/job_finder/data/models/job_model.dart
+++ hustlers_mobile/lib/features/job_finder/data/models/job_model.dart
@@ -1,34 +1,65 @@
 class JobModel {
-  final int id;
-  final String jobName;
+  final String jobTitle;
   final String jobType;
-  final String price;
-  final String deepLink;
-  final String jobDescription;
-  final String? expireDate;
-  final String? postedAt;
+  final String location;
+  final String sex;
+  final String salary;
+  final String deadline;
+  final String description;
+  final String moreInfo;
+  final String deeplink;
 
   JobModel({
-    required this.id,
-    required this.jobName,
+    required this.jobTitle,
     required this.jobType,
-    required this.price,
-    required this.deepLink,
-    required this.jobDescription,
-    this.expireDate,
-    this.postedAt,
+    required this.location,
+    required this.sex,
+    required this.salary,
+    required this.deadline,
+    required this.description,
+    required this.moreInfo,
+    required this.deeplink,
   });
 
   factory JobModel.fromJson(Map<String, dynamic> json) {
     return JobModel(
-      id: json['id'] as int,
-      jobName: json['jobname'] as String,
-      jobType: json['jobtype'] as String,
-      price: json['price'] as String,
-      deepLink: json['deep_link'] as String,
-      jobDescription: json['jobdescrbiton'] as String,
-      expireDate: json['expierdate'] as String?,
-      postedAt: json['posted_at'] as String?,
+      jobTitle: json['job_title'] as String? ?? '',
+      jobType: json['job_type'] as String? ?? '',
+      location: json['location'] as String? ?? '',
+      sex: json['sex'] as String? ?? '',
+      salary: json['salary'] as String? ?? '',
+      deadline: json['deadline'] as String? ?? '',
+      description: json['description'] as String? ?? '',
+      moreInfo: json['more_info'] as String? ?? '',
+      deeplink: json['deeplink'] as String? ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'job_title': jobTitle,
+      'job_type': jobType,
+      'location': location,
+      'sex': sex,
+      'salary': salary,
+      'deadline': deadline,
+      'description': description,
+      'more_info': moreInfo,
+      'deeplink': deeplink,
+    };
+  }
+
+  factory JobModel.fromMap(Map<String, dynamic> map) {
+    return JobModel(
+      jobTitle: map['job_title'] as String? ?? '',
+      jobType: map['job_type'] as String? ?? '',
+      location: map['location'] as String? ?? '',
+      sex: map['sex'] as String? ?? '',
+      salary: map['salary'] as String? ?? '',
+      deadline: map['deadline'] as String? ?? '',
+      description: map['description'] as String? ?? '',
+      moreInfo: map['more_info'] as String? ?? '',
+      deeplink: map['deeplink'] as String? ?? '',
     );
   }
 }
