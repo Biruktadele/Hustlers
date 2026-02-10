e4485245ee521254b019ee65e1367d5b3581070e
biruktadele <biruktadele55@gmail.com>
2026-02-10 08:11:31 +0300

    fete: implmentation of datasources company_remote_datasource.dart

--- hustlers_mobile/lib/features/job_finder/data/datasources/company_remote_datasource.dart
+++ hustlers_mobile/lib/features/job_finder/data/datasources/company_remote_datasource.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../models/company_insight_model.dart';
 import '../models/company_model.dart';
@@ -28,34 +29,29 @@ class CompanyRemoteDataSource {
       headers: {'accept': 'application/json'},
     );
     // debugPrint('Request URL: ${uri.toString()}');
+    debugPrint('🧠🧠🧠🧠🧠Response Body: ${response.body}');
 
     if (response.statusCode == 200) {
-      final List<dynamic> jsonResponse = json.decode(response.body);
-      return jsonResponse.map((json) => CompanyModel.fromJson(json)).toList();
+      final Map<String, dynamic> jsonResponse = json.decode(response.body);
+      final List<dynamic> data = jsonResponse['data'];
+      return data.map((json) => CompanyModel.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load companies: ${response.statusCode}');
     }
   }
 
   Future<CompanyInsightModel> getCompanyInsights({
-
-    required String location,
-    required String companyType,
-    required String searchType,
+    required CompanyModel company,
   }) async {
     final response = await client.post(
       Uri.parse('https://hustlers-production.up.railway.app/api/v1/map/company/insights'),
       headers: {'Content-Type': 'application/json'},
-      body: json.encode({
-        'location': location,
-        'company_type': companyType,
-        'search_type': searchType,
-      }),
+      body: json.encode(company.toJson()),
     );
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> jsonResponse = json.decode(response.body);
-      return CompanyInsightModel.fromJson(jsonResponse);
+      return CompanyInsightModel.fromJson(jsonResponse['data']);
     } else {
       throw Exception('Failed to load company insights: ${response.statusCode}');
     }
